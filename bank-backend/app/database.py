6a2442e6e871276c424f32da2dfa88071ec8f203
Mostafa-Hesham12 <eng.mostafa.hesham12@gmail.com>
2025-04-27 17:58:28 +0000

    database.py

--- bank-backend/app/database.py
+++ bank-backend/app/database.py
@@ -1,15 +1,7 @@
-from supabase import create_client, ClientOptions
+from supabase import create_client
 import os
 
-# Configure client options
-client_options = ClientOptions(
-    auto_refresh_token=False,
-    persist_session=False
-)
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_KEY")
 
-# Initialize Supabase client using Railway environment variables
-supabase = create_client(
-    os.environ["SUPABASE_URL"],  # سيقرأ مباشرة من Railway Variables
-    os.environ["SUPABASE_KEY"],  # سيقرأ مباشرة من Railway Variables
-    options=client_options
-)
+supabase = create_client(supabase_url, supabase_key)
